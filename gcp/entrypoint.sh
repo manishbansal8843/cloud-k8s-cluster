d79fdc7dc0ad8436683140527f0b41d7eeff9d08
Manish Bansal <manishbansal8843@gmail.com>
2020-02-12 21:14:44 +0530

    Added billing api call

--- gcp/entrypoint.sh
+++ gcp/entrypoint.sh
@@ -1,3 +1,5 @@
+gcloud components update
+gcloud components install alpha
 MY_PROJECT_ID=k8s-cluster-${NUM_OF_NODES}-nodes-$RANDOM
 gcloud projects create $MY_PROJECT_ID
 if [ $? -eq 1 ]; then
@@ -6,12 +8,15 @@ exit 1
 fi
 gcloud config set project $MY_PROJECT_ID
 export CLOUDSDK_COMPUTE_ZONE=us-east1-b
+BILLING_ACCOUNT_ID=$(gcloud alpha billing accounts list | tail -1 | cut -d' ' -f 1)
+gcloud alpha billing accounts projects link $MY_PROJECT_ID --account-id=$BILLING_ACCOUNT_ID
+gcloud services enable compute.googleapis.com
 gcloud compute instances create master-node \
     --image ubuntu-1604-xenial-v20200129 \
     --image-project ubuntu-os-cloud \
 	--create-disk size=50,type=pd-standard \
 	--machine-type n2-standard-2 \
-	--metadata-from-file startup-script=./install-scripts/gcp-install-master.sh
+	--metadata-from-file startup-script=gcp/install-scripts/gcp-install-master.sh
 
 if [ $NUM_OF_NODES -gt 1 ]; then
 WORKER_NODE=0
@@ -22,7 +27,7 @@ gcloud compute instances create worker-node-$WORKER_NODE \
     --image-project ubuntu-os-cloud \
 	--create-disk size=50,type=pd-standard \
 	--machine-type n2-standard-2 \
-	--metadata-from-file startup-script=./install-scripts/gcp-install-worker.sh
+	--metadata-from-file startup-script=gcp/install-scripts/gcp-install-worker.sh
 WORKER_NODE=$(($WORKER_NODE+1))
 done
 fi

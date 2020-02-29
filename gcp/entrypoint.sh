37d938e448054cfa1529c8fc69243cd9bbe9d63a
Manish Bansal <manishbansal8843@gmail.com>
2020-02-29 11:35:21 +0530

    Added comments

--- gcp/entrypoint.sh
+++ gcp/entrypoint.sh
@@ -1,5 +1,5 @@
-#gcloud components update
-gcloud components install alpha
+# Checking if project name is given to create k8s VMs. If not given, a new project will be created.
+# It is advisable to pass projectname as new projects will consume the limited projects quota.
 if [ -z $GCP_PROJECT_NAME ]
 then
   echo "GCP_PROJECT_NAME is not passed as an argument. Hence, will create a new project subject to the project quota availability of the user."
@@ -21,13 +21,19 @@ else
   echo "$GCP_PROJECT_NAME exists. Will be using it to create k8s VMs."
 fi
 
-
+# Setting compute zone, project id in the cloud shell
 gcloud config set project $MY_PROJECT_ID
 gcloud config set compute/zone us-east1-b
 export CLOUDSDK_COMPUTE_ZONE=us-east1-b
+
+# Enabling billing on the project to use compute services.
 BILLING_ACCOUNT_ID=$(gcloud alpha billing accounts list | tail -1 | cut -d' ' -f 1)
-gcloud alpha billing accounts projects link $MY_PROJECT_ID --account-id=$BILLING_ACCOUNT_ID
+gcloud beta billing projects link $MY_PROJECT_ID --billing-account=$BILLING_ACCOUNT_ID
+
+# Enabling compute services on the project to spawn new VMs
 gcloud services enable compute.googleapis.com
+
+# Instlling master node
 gcloud compute instances create master-node \
     --image ubuntu-1604-xenial-v20200223 \
     --image-project ubuntu-os-cloud \
@@ -35,24 +41,32 @@ gcloud compute instances create master-node \
 	--machine-type n1-standard-2 \
     --metadata username=$(whoami) \
 	--metadata-from-file startup-script=gcp/install-scripts/gcp-install-master.sh
+
+# SSH setup should have been done already on the cloud shell before executing ssh or scp
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa_cloud_k8s_cluster 2>/dev/null <<< y >/dev/null
-# SSH setup should have been done already on the cloud shell before executing scp
-while ! gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="[ -d ~/.kube ]"
+
+# Checking if master node installation is over
+while ! gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="[ -d ~/.kube ]" 2>/dev/null
 do
 echo "Master node installation is in progress. Sleeping for 30 seconds..."
 sleep 30
 done
-
+echo "Master node installed successfully!!"
 gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="kubectl get nodes"
-CLUSTER_TOKEN=$(gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="kubeadm token create")
+
+# Generating cluster token and ca cert hash from master node to be used by kubeadm on worker nodes
+CLUSTER_TOKEN=$(gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="kubeadm token create" 2>/dev/null)
 echo "Cluster token is $CLUSTER_TOKEN"
-gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="kubeadm token list"
 CLUSTER_DISCOVERY_TOKEN_CA_CERT_HASH=$(gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command="openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | \
   openssl dgst -sha256 -hex | sed 's/^.* //'")
 echo "Cluster ca cert hash is $CLUSTER_DISCOVERY_TOKEN_CA_CERT_HASH"
+
+# Getting master node internal IP to be used by kubeadm to join worker nodes
 K8S_MASTER_INTERNAL_IP=$(gcloud compute instances describe master-node \
   --format='get(networkInterfaces[0].networkIP)')
 echo "K8s master node internal ip is $K8S_MASTER_INTERNAL_IP"
+
+# Installing worker nodes
 if [ $NUM_OF_NODES -gt 1 ]; then
 WORKER_NODE=0
 while [ $WORKER_NODE -lt $(($NUM_OF_NODES-1)) ]
@@ -68,6 +82,7 @@ WORKER_NODE=$(($WORKER_NODE+1))
 done
 fi
 echo "###All worker nodes installed successfully###"
+
 echo "###Polling to check if all nodes are ready###"
 NUM_OF_READY_NODES=$(gcloud compute ssh master-node --ssh-key-file=~/.ssh/id_rsa_cloud_k8s_cluster --command=$'JSONPATH=\'{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}\'  && kubectl get nodes -o jsonpath="$JSONPATH" | grep -o "Ready=True" | wc -l')
 while [ $NUM_OF_READY_NODES -lt $NUM_OF_NODES ]
